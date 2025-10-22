2fd32b9a8c62109df7d43990785a42562ce2b6ce
FehFialho <fernandaklechowicz@gmail.com>
2025-10-22 09:02:54 -0300

    Get Data and Show Names

--- Divas-Gallery/src/App.jsx
+++ Divas-Gallery/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react';
 import './App.css'
 import { Card } from './components/Card'
+import axios from 'axios'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  //https://avaliacao-bosch.onrender.com/all
+
+  useEffect(() => {
+      getDivasData();
+  }, [])
+
+  const [divas, setDivas] = useState([])
+
+  const getDivasData = async() => {
+  const response = await axios.get("https://avaliacao-bosch.onrender.com/all")
+  setDivas(response.data)
+  console.log(response.data)
+  }
 
   return (
     <>
       <Card></Card>
-      {/* <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
+      
+      {
+        // divas.map(user => <><br></br><span>{user.name}</span></>)
+        
+        divas.map(divas => (
+          <li style={{ listStyle: "none", cursor:'pointer' }} key={divas.id}>
+          {divas.id} - {divas.nome}
+          </li>
+        ))
+      }
     </>
   )
 }
