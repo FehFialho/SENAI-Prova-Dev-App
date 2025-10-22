771aabbab34f89cc6ed6441e08097720c10b3075
FehFialho <fernandaklechowicz@gmail.com>
2025-10-22 09:38:26 -0300

    Melhorou, vai?

--- Divas-Gallery/src/App.jsx
+++ Divas-Gallery/src/App.jsx
@@ -23,18 +23,32 @@ function App() {
     <>
       {/* <Card></Card> */}
       
-      <section className='bg-red-600 m-10 p-2'>
+      <section className='bg-teal-400 m-10 p-2 flex flex-row flex-wrap w-9/10 justify-around'>
         {
           // divas.map(user => <><br></br><span>{user.name}</span></>)
-          
-          
-
           divas.map(divas => (
 
-            <div className='bg-red-400 m-2 h-60 w-60' key={divas.id}>
-              <span>{divas.id} - {divas.nome} {divas.sobrenome} </span>
-              <div></div>
-              <img className='w-50 max-h-50' src={divas.imagem} alt="" />
+            <div className='bg-teal-100 m-2 h-80 w-100 flex flex-col rounded-2xl' key={divas.id}>
+              
+              <img className='w-full h-65 rounded-t-2xl ' src={divas.imagem} alt="Personagem Favorito"/>
+              
+              {/* Data */}
+              <section className='flex flex-row w-full h-full gap-2 items-center justify-center'>
+                
+                <span>{divas.id} - {divas.nome} {divas.sobrenome} </span>
+
+                {/* Conditional Formatting! */}
+                {
+                  divas.apelido &&
+                  <span>( {divas.apelido} )</span>             
+                }
+                {
+                  divas.status == 'Matriculado' &&
+                  <div className='bg-green-400 rounded-full w-6 h-6'></div>                
+                }
+                
+              </section>
+
             </div>
 
             // <li style={{ listStyle: "none", cursor:'pointer' }} key={divas.id}>
